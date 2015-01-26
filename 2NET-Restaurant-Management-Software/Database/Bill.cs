694a7c28122f9c72523de4601b9cb8014e8977bb
cBeal <169796@supinfo.com>
2015-01-26 17:53:03 +0100

    ajout de bill

---
+++ 2NET-Restaurant-Management-Software/Database/Bill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2NET_Restaurant_Management_Software.Database
+{
+    class Bill
+    {
+        public int BillId { get; set; }
+
+        public int WaiterId { get; set; }
+
+        public virtual Waiter Waiter { get; set; }
+
+        public virtual List<Meal> Meals { get; set; }
+    }
+}
