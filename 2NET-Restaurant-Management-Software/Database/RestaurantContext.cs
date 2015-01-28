4d64647d38ed40210ffd7948cdbeff0ab8ac1e62
cBeal <169796@supinfo.com>
2015-01-28 13:00:28 +0100

    Ajout du context

---
+++ 2NET-Restaurant-Management-Software/Database/RestaurantContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace _2NET_Restaurant_Management_Software.Database
+{
+    class RestaurantContext : DbContext
+    {
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<Waiter> Waiters { get; set; }
+    }
+}
