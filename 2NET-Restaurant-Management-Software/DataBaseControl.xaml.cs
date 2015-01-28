da3f9bc8c03a38b1f952dd7d666bb45c366d23b2
cBeal <169796@supinfo.com>
2015-01-28 12:14:38 +0100

    L'onglet Waiter possede la creation, suppression et modification

--- 2NET-Restaurant-Management-Software/DataBaseControl.xaml.cs
+++ 2NET-Restaurant-Management-Software/DataBaseControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using _2NET_Restaurant_Management_Software.Database;
 
 namespace _2NET_Restaurant_Management_Software
 {
@@ -20,14 +21,111 @@ namespace _2NET_Restaurant_Management_Software
     /// </summary>
     public partial class DataBaseControl : UserControl
     {
+        private RestaurantContext database;
         public DataBaseControl()
         {
             InitializeComponent();
+            database = new RestaurantContext();
+            BindWaiterComboBox();
+        }
+
+        public void BindWaiterComboBox()
+        {
+            var query = from b in database.Waiters
+                        select b;
+
+            ComboTest.ItemsSource = query.ToList();
+            ComboTest.DisplayMemberPath = "FirstName";
+            ComboTest.SelectedValuePath = "WaiterId";
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            var waiter = new Waiter
+            {
+                FirstName = FirstName.Text,
+                LastName = LastName.Text,
+
+            };
+
+            database.Waiters.Add(waiter);
+            database.SaveChanges();
+            Status.Content = "Waiter created";
+            BindWaiterComboBox();
+        }
+
+        private void DeleteWaiter(object sender, RoutedEventArgs e)
+        {
+            int index = (int)ComboTest.SelectedValue;
+
+            if (index > 0)
+            {
+                var query = from b in database.Waiters
+                            where b.WaiterId == index
+                            select b;
+
+                foreach (var item in query)
+                {
+                    database.Waiters.Remove(item);
+                }
+
+                database.SaveChanges();
+                BindWaiterComboBox();
+            }
         }
 
         private void ChangeControl(object sender, RoutedEventArgs e)
         {
             this.Content = new MainControl();
+        }
+
+        private void LoadWaiter(object sender, ContextMenuEventArgs e)
+        {
+            BindWaiterComboBox();
+        }
+
+        private void UpdateBox(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboTest.SelectedValue != null)
+            {
+                int index = (int)ComboTest.SelectedValue;
+
+                if (index > 0)
+                {
+                    var query = from b in database.Waiters
+                                where b.WaiterId == index
+                                select b;
+
+                    foreach (var item in query)
+                    {
+                        FirstName.Text = item.FirstName;
+                        LastName.Text = item.LastName;
+                    }
+                }
+            }
+        }
+
+        private void UpdateWaiter(object sender, RoutedEventArgs e)
+        {
+            if (ComboTest.SelectedValue != null)
+            {
+                int index = (int)ComboTest.SelectedValue;
+
+                if (index > 0)
+                {
+                    var query = from b in database.Waiters
+                                where b.WaiterId == index
+                                select b;
+
+                    foreach (var item in query)
+                    {
+                        item.FirstName = FirstName.Text;
+                        item.LastName = LastName.Text;
+                    }
+
+                    database.SaveChanges();
+                }
+            }
         }
     }
 }
--- 2NET-Restaurant-Management-Software/MainControl.xaml.cs
+++ 2NET-Restaurant-Management-Software/MainControl.xaml.cs
@@ -26,52 +26,6 @@ namespace _2NET_Restaurant_Management_Software
         {
             InitializeComponent();
             database = new RestaurantContext();
-            BindComboBox();
-        }
-
-        public void BindComboBox()
-        {
-            var query = from b in database.Waiters
-                        select b;
-
-            ComboTest.ItemsSource = query.ToList();
-            ComboTest.DisplayMemberPath = "FirstName";
-            ComboTest.SelectedValuePath = "WaiterId";
-        }
-
-        private void OnClick(object sender, RoutedEventArgs e)
-        {
-            var waiter = new Waiter
-            {
-                FirstName = FirstName.Text,
-                LastName = LastName.Text,
-
-            };
-
-            database.Waiters.Add(waiter);
-            database.SaveChanges();
-            Status.Content = "Waiter created";
-            BindComboBox();
-        }
-
-        private void DeleteWaiter(object sender, RoutedEventArgs e)
-        {
-            int index = (int)ComboTest.SelectedValue;
-
-            if (index > 0)
-            {
-                var query = from b in database.Waiters
-                            where b.WaiterId == index
-                            select b;
-
-                foreach (var item in query)
-                {
-                    database.Waiters.Remove(item);
-                }
-
-                database.SaveChanges();
-                BindComboBox();
-            }
         }
 
         private void GoToDatabase(object sender, RoutedEventArgs e)
