e58a5f4121cc0bbe76b69804305748ed8d8d9fec
cBeal <169796@supinfo.com>
2015-02-01 10:34:33 +0100

    Ajout et test de la creation de bouton via le code

--- Restaurant/MainControl.xaml.cs
+++ Restaurant/MainControl.xaml.cs
@@ -26,6 +26,7 @@ namespace Restaurant
         {
             InitializeComponent();
             database = new RestaurantContext();
+            LoadTables();
         }
 
         private void GoToDatabase(object sender, RoutedEventArgs e)
@@ -33,6 +34,23 @@ namespace Restaurant
             {
                 this.Content = new DataBaseControl();
             }
+        }
+
+        private void LoadTables(){
+            var query = from b in database.Tables
+                        select b;
+
+            foreach (var item in query)
+            {
+                //Create new button
+            }
+        }
+
+        private void Addtable(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button newBtn = new Button();
+            newBtn.Content = "A New Button";
+            Wrap.Children.Add(newBtn);
         }
     }
 }
